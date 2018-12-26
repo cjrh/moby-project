5a49572cf10638e0b571da33418b093394d30c5a
Caleb Hattingh <caleb.hattingh@gmail.com>
2018-12-26 21:35:48 +1000

    Simplified to a single word.

--- thesauromatic/source/app.d
+++ thesauromatic/source/app.d
@@ -3,6 +3,7 @@ import std.string;
 import std.getopt;
 import std.algorithm.setops;
 import std.typecons;
+import std.array;
 
 string word;
 
@@ -18,33 +19,56 @@ void main(string[] args)
                 "Usage: thesauromatic <word>\n\n" ~
                 "Given <word> this program will print out a list\n" ~
                 "of synonyms, one per line. This makes it easy to\n" ~
-                "consume with other tools.",
+                "consume with other tools.\n",
                 helpInformation.options);
     }
 
-    if (args.length == 2) {
+    if (args.length >= 2) {
         writeln(syns(args[1]));
-    } else if (args.length == 3) {
-        writeln(
-            setIntersection(syns(args[1]), syns(args[2]))
-        );
+    } else {
+        writeln("ERROR: At least one word must be specified.");
     }
 }
 
-static string[][string] process() {
+static string[string] process() {
     const string raw = import("mobythes.aur");
     auto lines = splitLines(raw);
-    /* writeln(lines[0..5]); */
-    string[][string] result;
+    string[string] result;
     foreach (line; lines) {
         auto items = line.split(",");
-        result[items[0]] = items[1..$];
+        result[items[0]] = items[1..$].join("\n");
     }
     return result;
 }
 
-string[] syns(string word) {
+private string syns(string word) {
     auto x = process();
-    /* writeln(x[word]); */
     return x[word];
 }
+
+
+/*
+>>> with open('mobythes.aur') as f:
+...     rows = f.readlines()
+
+>>> len(rows)
+30260
+
+>>> from pprint import pprint
+
+>>> from collections import Counter
+
+>>> from itertools import chain
+
+>>> c = Counter(chain.from_iterable(r.split(',') for r in rows))
+
+>>> list(c.keys())[:10]
+['a cappella', 'abbandono', 'accrescendo', 'affettuoso', 'agilmente', 'agitato', 'amabile', 'amoroso', 'appassionatamente', 'appassionato']
+
+>>> c.most_common(10)
+[('cut', 1120), ('set', 919), ('run', 749), ('turn', 703), ('check', 699), ('break', 697), ('line', 678), ('cast', 653), ('close', 645), ('pass', 633)]
+
+>>> len(c.keys())
+107980
+
+*/
