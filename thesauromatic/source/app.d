0474fac8a15418a7fe3e66b71777f6cff628f546
Caleb Hattingh <caleb.hattingh@gmail.com>
2018-12-30 02:54:23 +1000

    Got a much faster version.

--- thesauromatic/source/app.d
+++ thesauromatic/source/app.d
@@ -3,20 +3,24 @@ import std.string;
 import std.getopt;
 import std.algorithm.setops;
 import std.algorithm.iteration;
+import std.algorithm.searching;
 import std.typecons;
 import std.array;
 import std.range;
 import std.file: slurp;
+import std.conv;
+import std.format;
 
-string word;
-/* private static string raw = import("mobythes.aur"); */
-/* private static string wordsonly = import("wordsonly.txt"); */
+private immutable string mobylf = import("mobylf.txt");
+static immutable string[string] data;
 
-/* string[][string] synonyms; */
-/*  */
-/* static this() { */
-/*     synonyms = mixin(import("readymixin.txt")); */
-/* } */
+static this() {
+    Tuple!(string, string, string) r;
+    foreach (line; mobylf.lineSplitter()) {
+        r = line.findSplit(",");
+        data[r[0]] = r[2];
+    }
+}
 
 void main(string[] args)
 {
@@ -29,6 +33,7 @@ void main(string[] args)
                 "of synonyms, one per line. This makes it easy to\n" ~
                 "consume with other tools.\n",
                 helpInformation.options);
+        return;
     }
 
     if (args.length >= 2) {
@@ -39,54 +44,8 @@ void main(string[] args)
     }
 }
 
-private static string[string] process() {
-    auto s = slurp!(string, string)("source/mobylf.txt", "%s,%s");
-    /* auto lines = splitLines(raw); */
-    string[string] result;
-    /* auto f5 = File("readymixin.txt", "w"); */
-    /* f5.writeln("["); */
-    foreach (tup; s) {
-        result[tup[0]] = tup[1];
-        /* f5.writefln( */
-        /*     "\"%s\": [%s],", */
-        /*     items[0], map!(a => "\"" ~ a ~ "\"")(items[1..$]).join(",") */
-        /* ); */
-    }
-    /* f5.writeln("]"); */
-    return result;
-}
-
 private string syns(const string word) {
-    auto x = process();
-    if (word in x)
-        return x[word].replace(",", "\n");
-        /* return map(x[word].join("\n"); */
-    return "";
+    if ((word in data) is null)
+        return "";
+    return data[word].replace(",", "\n");
 }
-
-
-/*
->>> with open('mobythes.aur') as f:
-...     rows = f.readlines()
-
->>> len(rows)
-30260
-
->>> from pprint import pprint
-
->>> from collections import Counter
-
->>> from itertools import chain
-
->>> c = Counter(chain.from_iterable(r.split(',') for r in rows))
-
->>> list(c.keys())[:10]
-['a cappella', 'abbandono', 'accrescendo', 'affettuoso', 'agilmente', 'agitato', 'amabile', 'amoroso', 'appassionatamente', 'appassionato']
-
->>> c.most_common(10)
-[('cut', 1120), ('set', 919), ('run', 749), ('turn', 703), ('check', 699), ('break', 697), ('line', 678), ('cast', 653), ('close', 645), ('pass', 633)]
-
->>> len(c.keys())
-107980
-
-*/
